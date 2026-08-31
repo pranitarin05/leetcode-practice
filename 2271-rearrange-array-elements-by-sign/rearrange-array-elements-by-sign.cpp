682e0d39e2ec9cc9492965a818393a7a168e5f5a
pranitarin05 <149330332+pranitarin05@users.noreply.github.com>
2026-08-31 17:52:25 +0530

    Time: 7 ms (55.86%) | Memory: 128.2 MB (95.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -5,7 +5,7 @@ public:
         vector<int>ans(n);
         int pos=0;
         int neg=1;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             if(nums[i]>0){
                 ans[pos]=nums[i];
                 pos += 2;
